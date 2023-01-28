6ca7662047e25cbd85dddd4ac011b145f9ba0da3
Adham <muzaffarov.adham@gmail.com>
2023-01-28 13:38:51 +0500

    feat: add middleware

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,13 +1,27 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, Middleware } from '@reduxjs/toolkit';
+import saga from 'redux-saga';
 import stepsReducers from './stepsSlice';
 import userReducers from './userSlice';
+import sagas from './sagas';
+
+const loggerMiddleware: Middleware = (store) => (next) => (action) => {
+  console.log('Action:', action);
+  const result = next(action);
+  console.log('Next state:', store.getState());
+  return result;
+};
+
+const sagaMiddleware = saga();
 
 export const store = configureStore({
   reducer: {
     steps: stepsReducers,
     user: userReducers,
   },
+  middleware: [loggerMiddleware, sagaMiddleware],
 });
+
+sagaMiddleware.run(sagas);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
